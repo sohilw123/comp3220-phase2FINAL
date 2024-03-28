46e2cfc276d02ecf15f929464ffd5930af34db29
sohilw123 <124415777+sohilw123@users.noreply.github.com>
2024-03-28 01:12:42 -0400

    Update Dataset.java

--- src/main/java/Dataset.java
+++ src/main/java/Dataset.java
@@ -1,10 +1,19 @@
+/**
+ * Represents a dataset.
+ */
 public class Dataset {
   private String name;
   private String description;
   private String source;
   // private Frequency updateFrequency;
 
-  // Constructor
+  /**
+   * Constructs a Dataset object.
+   *
+   * @param name        The name of the dataset.
+   * @param description The description of the dataset.
+   * @param source      The source of the dataset.
+   */
   public Dataset(String name, String description, String source) {
     this.name = name;
     this.description = description;
@@ -12,7 +21,12 @@ public class Dataset {
     // this.updateFrequency = updateFrequency;
   }
 
-  // Method to parse dataset from CSV format
+  /**
+   * Parses dataset from CSV format.
+   *
+   * @param csvLine The CSV line representing the dataset.
+   * @return The Dataset object parsed from the CSV line.
+   */
   public static Dataset fromCSV(String csvLine) {
     String[] data = csvLine.split(",");
     String name = data[0];
@@ -22,7 +36,11 @@ public class Dataset {
     return new Dataset(name, description, source);
   }
 
-  // Method to convert dataset to CSV format
+  /**
+   * Converts dataset to CSV format.
+   *
+   * @return The CSV representation of the dataset.
+   */
   public String toCSV() {
       return name + "," +
             description + "," +
@@ -30,6 +48,11 @@ public class Dataset {
             "placeholder";
   }
 
+  /**
+   * Generates a string representation of the dataset.
+   *
+   * @return The string representation of the dataset.
+   */
   @Override
   public String toString() {
     return "Dataset{" +
@@ -40,26 +63,57 @@ public class Dataset {
   }
 
   // Getter and setter methods
+
+  /**
+   * Retrieves the name of the dataset.
+   *
+   * @return The name of the dataset.
+   */
   public String getName() {
     return name;
   }
 
+  /**
+   * Sets the name of the dataset.
+   *
+   * @param name The name to set for the dataset.
+   */
   public void setName(String name) {
     this.name = name;
   }
 
+  /**
+   * Retrieves the description of the dataset.
+   *
+   * @return The description of the dataset.
+   */
   public String getDescription() {
     return description;
   }
 
+  /**
+   * Sets the description of the dataset.
+   *
+   * @param description The description to set for the dataset.
+   */
   public void setDescription(String description) {
     this.description = description;
   }
 
+  /**
+   * Retrieves the source of the dataset.
+   *
+   * @return The source of the dataset.
+   */
   public String getSource() {
     return source;
   }
 
+  /**
+   * Sets the source of the dataset.
+   *
+   * @param source The source to set for the dataset.
+   */
   public void setSource(String source) {
     this.source = source;
   }
