db0eca9a5eb900b8e51ac383c304622432f1c9bb
sohilw123 <124415777+sohilw123@users.noreply.github.com>
2024-03-28 01:15:31 -0400

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,14 @@
 import java.util.Scanner;
 
+/**
+ * The main class of the program.
+ */
 public class Main {
+    /**
+     * The entry point of the program.
+     *
+     * @param args The command-line arguments.
+     */
     public static void main(String[] args) {
         Program program = new Program();
         program.start();
