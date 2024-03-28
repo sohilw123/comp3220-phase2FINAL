5d93626dece6276aff0cbd88e08d9f5552696f9c
sohilw123 <124415777+sohilw123@users.noreply.github.com>
2024-03-28 01:14:46 -0400

    Update DatasetObserver.java

--- src/main/java/DatasetObserver.java
+++ src/main/java/DatasetObserver.java
@@ -1,3 +1,9 @@
+/**
+ * An interface for objects that observe changes in datasets.
+ */
 public interface DatasetObserver {
+    /**
+     * Called to notify the observer about an update in the dataset.
+     */
     void update();
 }
